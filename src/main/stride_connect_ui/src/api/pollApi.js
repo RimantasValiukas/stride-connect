240d1c0222c8a2440d65b260fb5d863b3e48c231
RimantasValiukas <rimantas.valiukas@gmail.com>
2024-01-28 20:24:01 +0200

    [UI] To Polls component added card for rendering polls.

--- src/main/stride_connect_ui/src/api/pollApi.js
+++ src/main/stride_connect_ui/src/api/pollApi.js
@@ -1,7 +1,9 @@
 import HTTP from "./index";
 
 const createPoll = (poll) => HTTP.post('/api/polls/create', poll);
+const getPolls = () => HTTP.get("/api/polls");
 
 export {
-    createPoll
+    createPoll,
+    getPolls
 }
--- src/main/stride_connect_ui/src/components/page/Polls.js
+++ src/main/stride_connect_ui/src/components/page/Polls.js
@@ -1,13 +1,52 @@
-import {Button} from "react-bootstrap";
+import {Button, Card, Col, FormText, Row} from "react-bootstrap";
+import {useEffect, useState} from "react";
+import {getPolls} from "../../api/pollApi";
 
 const Polls = () => {
 
+    const [polls, setPolls] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getPolls()
+            .then(({data}) => {
+
+                console.log(data);
+                setPolls(data);
+            })
+            .catch((error) => console.log(error))
+            .finally(() => setLoading(false))
+        }, []);
+
     return(
         <div className="mb-2">
-            <Button href='/poll' variant="primary" size="lg" style={{backgroundColor: '#43535f', borderColor: '#43535f',}}>
-                Sukurti balsavimą
-            </Button>
+            <Row className="justify-content-center align-items-center">
+                <Button href='/poll' size="lg" style={{backgroundColor: '#43535f', borderColor: '#43535f', marginBottom: '20px', maxWidth: '190px'}}>
+                    Sukurti balsavimą
+                </Button>
+            </Row>
+
+            <FormText style={{fontSize: '25px', marginBottom: '60px'}}>Aktyvūs balsavimai, atiduok savo balsą</FormText>
+
+            <Row xs={1} md={1} className="g-4 mt-2">
+                {Array.from(polls).map((poll, idx) => (
+                    <Col key={idx}>
+                        <Card>
+                            <Card.Header className="ml-2 d-flex justify-content-start text-left" style={{backgroundColor: '#dcbc7c'}}><i>Balsavimas sukurtas:</i>&nbsp; {new Date(poll.date).toLocaleString()}, &nbsp;<i>Autorius:</i>&nbsp; Vardenis Pavardenis</Card.Header>
+                            <Card.Body style={{backgroundColor: '#f3f3eb'}}>
+                                <Card.Title>Balsavimo pavadinimas: {poll.name}</Card.Title>
+                                <Card.Text className="mx-auto" style={{maxWidth: '1000px', textAlign: 'justify'}}>
+                                    {poll.description}
+                                </Card.Text>
+                                <Button size="sm" style={{backgroundColor: '#9dab9d', borderColor: '#9dab9d'}}>Balsuoti</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </div>
+
+
     );
 }
 export default Polls;
