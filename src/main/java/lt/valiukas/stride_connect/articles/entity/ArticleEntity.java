e6e57c3aafbb93bc00fb3cdbf47792acd2879c5b
RimantasValiukas <rimantas.valiukas@gmail.com>
2024-02-24 15:31:29 +0200

    [API] changed table name

--- src/main/java/lt/valiukas/stride_connect/articles/entity/ArticleEntity.java
+++ src/main/java/lt/valiukas/stride_connect/articles/entity/ArticleEntity.java
@@ -1,9 +1,6 @@
 package lt.valiukas.stride_connect.articles.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +10,7 @@ import lt.valiukas.stride_connect.articles.dto.Article;
 import java.sql.Timestamp;
 import java.util.UUID;
 
-@Entity
+@Entity(name = "articles")
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
